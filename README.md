4801fe881bec97f2299923e81d88036717597dda
Jacob West <jacobwest@Jacobs-MacBook-Pro-2.local>
2026-07-26 22:53:52 -0600

    Simplest version

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Code Division Multiple Access (CDMA)
+
+## README TODO
+
+- [x] Defining CDMA
+- [ ] Setting parameters for testing; define success for the project
+- [ ] Track benchmarks and progress
+- [ ] DOD
+
+## PROJECT TODO
+
+- [ ] Set up testing harness (including test 'users' and their messages. Whatever the 'message' it will need to be transmitted into raw binary before it's processed)
+- [ ] Assign PN (psuedo-random noise) to each 'user'
+- [ ] Utilizing each PN, 'spread' each message
+- [ ] Combine all spread messages
+- [ ] Implement encryption? AES of some kind, elliptical is probably the best one (used in modern phones now)
+
+## Steps
+
+- Initially, each 'user' (which would be a single device, like a phone) is assigned a unique identifier code
+- This code is utilized to 'spread' the users message across a spectrum
+- Multiple messages are combined together and transmitted together
+- When the message reaches the intended target, the user 'decodes' the message to obtain the message intended for them from the other messages included
+
+## Encoding
+
+When transmitting a message through a cellular network, the CDMA encodes each bit of the message.
+
+- 1 -> +1
+- 0 -> -1
+
+0 is reserved to mean 'no transmission' by the network.
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <iterator>
+
 int main()
 {
+    // Set the number of users
+    int numUsers = 1;
+
+    // Set a simple spreading code for our single user
+    int spreadingCode[2] = {-1, 1};
+
+    // Create the initial message
+    int message = 1;
+
+    int messageConverted = message ? 1 : -1;
+
+    std::cout << messageConverted << std::endl;
+
+    // Spread the message across the spectrum
+    int spreadMessage[sizeof(spreadingCode) / sizeof(spreadingCode[0])] = {};
+    for (int i = 0; i < sizeof(spreadingCode) / sizeof(spreadingCode[0]); i++)
+    {
+        spreadMessage[i] = spreadingCode[i] * messageConverted;
+        std::cout << spreadMessage[i] << " ";
+    }
+    std::cout << "\n";
+
+    // Now that we have the message spread, we can decode it
+    int resultMessage = 0;
+
+    for (int i = 0; i < sizeof(spreadingCode) / sizeof(spreadingCode[0]); i++)
+    {
+        resultMessage += spreadMessage[i] * spreadingCode[i];
+    }
+
+    int chipLength = sizeof(spreadingCode) / sizeof(spreadingCode[0]);
+    int decoded = resultMessage / (numUsers * chipLength); // gives -1 or +1
+
+    // Output!
+    int recoveredMessage = (decoded == 1) ? 1 : 0;
+    std::cout << "Recovered: " << recoveredMessage << std::endl;
 }
